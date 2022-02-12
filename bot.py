027a89b4573c11505c0e0dfc0165449ca0a912e3
PaulMndn <paul.m.menden@gmail.com>
2022-02-12 20:38:19 +0100

    first commands (game, player search)

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import discord
-from discord import Option
+from discord import Embed, Option
 from discord.ext import commands
-from discord.ext.commands.bot import Bot
-
+from discord.ext.commands import Bot
+import asyncio
 import logging
 from logging.handlers import RotatingFileHandler
 
-logging.getLogger("discord").level=logging.INFO
+import vrml
 
+
+logging.getLogger("discord").level=logging.INFO
 handler = RotatingFileHandler(filename="./log/VRMLbot.log",
                               backupCount=3,
                               maxBytes=100 * 1024,
@@ -17,6 +19,10 @@ logging.basicConfig(level=logging.DEBUG,
                     format="{asctime} - {levelname:<8} - {name}: {message}",
                     style="{")
 log = logging.getLogger("main")
+
+
+with open("bot.token", "r") as f:
+    token = f.read().strip()
 
 
 debug_guilds = [927322319691591680]
@@ -33,5 +39,36 @@ async def ping(ctx):
     await ctx.respond("Pong!")
 
 
+@bot.slash_command()
+async def game(ctx,
+               game: Option(str, "game name", choices=["Echo Arena","Onward","Pavlov","Snapshot","Contractors","Final Assult"])):
+    "Get general information of a game in VRML."
+    game = game.replace(" ", "")
+    game: vrml.Game = await vrml.get_game(game)
+    await ctx.respond(f"{game.name} ({game.id}): can be found here: <{game.url}>")
 
-bot.run("")
+
+@bot.slash_command()
+async def player(ctx,
+                 name: Option(str, "Name of the player"),
+                 game: Option(str, "game name", choices=["Echo Arena","Onward","Pavlov","Snapshot","Contractors","Final Assult"])):
+    await ctx.defer()   # buying some time
+
+    found_players = await vrml.player_search(name)
+    fetch_tasks = [bot.loop.create_task(player.fetch()) for player in found_players]
+    players = await asyncio.gather(*fetch_tasks)
+    players = list(filter(lambda p:p.game.name == game, players))
+    
+    if not len(players):
+        await ctx.respond("No player found.")
+        return
+    
+    embeds = []
+    for player in players:
+        e = Embed(title=f"`{player.name}`", description=f"`{player.user.discord_tag}`")
+        e.set_thumbnail(url=player.logo_url)
+        embeds.append(e)
+    await ctx.respond("", embeds=embeds)
+
+
+bot.run(token)
