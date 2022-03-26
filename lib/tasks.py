1c59c3be6c66a3bf4c6773243b2bcf54e2970f06
PaulMndn <paul.m.menden@gmail.com>
2022-03-26 13:41:15 +0100

    catch TypeError raised by an unexpected API response. 5 tries of player is skipped.

--- lib/tasks.py
+++ lib/tasks.py
@@ -14,8 +14,8 @@ log = logging.getLogger(__name__)
 
 @loop(time=time(hour=0, minute=0))
 async def fetch_vrml_discord_player(force=False):
-    """Separate for each game in VRML, the discord handle with corresponding 
-    player_id and team_id and team rank are fetched. 
+    """Separate for each game in VRML, the discord handle with 
+    corresponding player_id and team_id and team rank are fetched.
     
     Only fetched on Monday 00:00 UTC"""
     if not force and datetime.now(tz=timezone.utc).weekday != 0:
@@ -33,10 +33,18 @@ async def fetch_vrml_discord_player(force=False):
         for i in range(0, len(p_players), 10):
             # fetching 10 players at once to speed up but not overload
             # the API
-            tasks = [
-                asyncio.create_task(p.fetch()) for p in p_players[i: i+10]
-            ]
-            players += await asyncio.gather(*tasks)
+            async def fetch_player(p_player):
+                for i in range(5):
+                    try:
+                        return await p_player.fetch()
+                    except TypeError:
+                        # Catch weird API response returning `None` instead
+                        # of `dict` give it 5 tries or return `None`.
+                        continue
+                
+            coros = [fetch_player(p) for p in p_players[i: i+10]]
+            ret = await asyncio.gather(*coros)
+            players += [p for p in ret if p is not None]
         for player in players:
             id = player.user.discord_id
             if id is None:
